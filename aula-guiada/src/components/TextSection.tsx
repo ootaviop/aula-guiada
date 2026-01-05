e4ba58ce4ed281a199b4b9463f771c39acb94ffb
ootaviop <otavio.p.work@gmail.com>
2026-01-05 20:35:30 -0300

    save

--- aula-guiada/src/components/TextSection.tsx
+++ aula-guiada/src/components/TextSection.tsx
@@ -1,9 +1,13 @@
 import '../styles/TextSection.css';
+import Paragraph from './Paragraph';
 
 const TextSection = () => {
     return (
             <div className="text-section">
-
+                <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Paragraph>
+                <Paragraph>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Paragraph>
+                <Paragraph>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</Paragraph>
+                <Paragraph>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Paragraph>
             </div>
     );
 }
